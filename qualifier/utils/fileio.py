05d4351e1f9eddc48b6d36e85bb396e7b1bfc2ed
Mark Beers <126819603+cannabbeers@users.noreply.github.com>
2023-05-05 13:38:49 -0700

    functionality mods requested BizOps acceptance criteria noted and tested

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -4,9 +4,10 @@
 This contains a helper function for loading and saving CSV files.
 
 """
-import csv
-from pathlib import Path
 
+import csv
+# imported Path to allow 'save_csv' function to open a path to a new .csv file so UI allows saving loan_qualifier results if desired
+from pathlib import Path
 
 def load_csv(csvpath):
     """Reads the CSV file from path provided.
@@ -18,6 +19,7 @@ def load_csv(csvpath):
         A list of lists that contains the rows of data from the CSV file.
 
     """
+    
     with open(csvpath, "r") as csvfile:
         data = []
         csvreader = csv.reader(csvfile, delimiter=",")
@@ -30,6 +32,7 @@ def load_csv(csvpath):
             data.append(row)
     return data
 
+# defined 'save_csv' function in utils folder so app.py can call it when requested by user running program - maintaining DRY modular coding style
 def save_csv(data, loan_list):
     """Creating a new function called save_csv that saves a csv file
     Args:
@@ -38,6 +41,7 @@ def save_csv(data, loan_list):
     Returns:
         A csv file with the saved data
     """   
+    
     bank_data = data
 
     #Creating headers for the csv file
@@ -56,64 +60,3 @@ def save_csv(data, loan_list):
 
     return data
 
-# code below needed to define 'save_csv' function used in 'loan_qualifier_app_v2.py'
-# def save_csv(filtered_loans, loan_data):
-#     with open(filtered_loans, 'w') as app_output:
-#         write = csv.writer(app_output)
-#         write.writerows(loan_data)
-
-# # def save_csv("pre_qual.csv", newline='') as f:
-# #     csvfile = 
-
-# # # def save_csv(csvpath, data, header=None):
-# # #     """Saves the CSV file from path provided.
-
-# # #     Args:
-# # #         csvpath (Path): The CSV file path.
-# # #         data (list of lists): A list of the rows of data for the CSV file.
-# # #         header (list): An optional header for the CSV.
-
-# # #     """
-# # #     with open(csvpath, "w", newline="") as csvfile:
-# # #         csvwriter = csv.writer(csvfile, delimiter=',')
-# # #         if header:
-# # #             csvwriter.writerow(header)
-# # #         csvwriter.writerows()
-
-
-
-
-
-
-
-
-# # # code to define 'save_csv' function - located here to maintain modular style
-# # # def save_csv(filtered_loans, loan_data):
-# # #     with open(filtered_loans, 'w') as app_output:
-# # #         write = csv.writer(app_output)
-# # #         write.writerows(loan_data)
-
-# # # define function(Path, data, header)
-
-# # # def save_csv(qual_loans.csv, loan_data, header=None):
-
-# # #     with open(qual_loans.csv, 'w', newline="") as prequal_csvfile:
-# # #         writer = csv.writer(prequal_csvfile, delimiter=',')
-# # #         if header:
-# # #             writer.writerow(header)
-# # #         writer.writerows(loan_data)
-
-# # # def save_csv(csvpath, data, header=None):
-# # #     """Saves the CSV file from path provided.
-
-# # #     Args:
-# # #         csvpath (Path): The CSV file path.
-# # #         data (list of lists): A list of the rows of data for the CSV file.
-# # #         header (list): An optional header for the CSV.
-
-# # #     """
-# # #     with open(csvpath, "w", newline="") as csvfile:
-# # #         csvwriter = csv.writer(csvfile, delimiter=',')
-# # #         if header:
-# # #             csvwriter.writerow(header)
-# # #         csvwriter.writerows(data)
