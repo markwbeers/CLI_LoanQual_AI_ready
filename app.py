a029593dce94badc07289c84820f2dc021317b07
Mark Beers <126819603+cannabbeers@users.noreply.github.com>
2023-05-05 21:04:23 -0700

    app.py fully functional, nearly complete

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ Example:
     $ python app.py
 """
 import sys
-import fire
-import questionary as qst
-from pathlib import Path
+import fire # imported fire library to enable python object conversion to CLI creation requested by BizOps Team
+import questionary as qst # imported questionary as 'qst' library required to write simple CLI's with shortened code
+from pathlib import Path # path module creates strings that represent file paths
 
-from qualifier.utils.fileio import load_csv
-from qualifier.utils.fileio import save_csv
+from qualifier.utils.fileio import load_csv, save_csv
+# from qualifier.utils.fileio import save_csv <-- added then replaced line#15 with simpler import code - ie. ...", save_csv" addition to line 14
 
 from qualifier.utils.calculators import (
     calculate_monthly_debt_ratio,
@@ -24,15 +24,14 @@ from qualifier.filters.credit_score import filter_credit_score
 from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value import filter_loan_to_value
 
-# code added to improve UI appearance
-print("\n")
+print("\n") # code added to improve UI output appearance/spacing in terminal output
 
 def underline(text):
     print("\u0332".join(text))
 
-underline("Welcome to the UCB FinTech Desktop Underwriter Terminal!\n")
+underline("Welcome to the UCB FinTech Desktop Underwriter Terminal!\n") # added welcome text for more attractive UI
 print("Thank you for using our Home Equity Line of Credit (HELOC) pre-qual screening portal.\n")
-print("\t Today's Bank Loan Offerings located here --> 'data/rates.csv' <-- (enter below)\n")
+print("\t Today's Bank Loan Offerings located here --> 'data/rates.csv' <-- (enter below)\n") # made shortened .csv filename - improves ease of use
 
 def load_bank_data():
     """Ask for the file path to the latest banking data and load the CSV file.
@@ -41,13 +40,12 @@ def load_bank_data():
         The bank data from the data rate sheet CSV file.
     """
 
-    csvpath_rates = qst.text("Enter a file path to a rate-sheet (.csv):").ask()
+    csvpath_rates = qst.text("Enter the file path above for today's bank rate-sheet (.csv):").ask() # improved ease of use, clarified instruction wording
     csvpath_rates = Path(csvpath_rates)
     if not csvpath_rates.exists():
         sys.exit(f"Oops! Can't find this path: {csvpath_rates} - please run program again when ready...")
 
     return load_csv(csvpath_rates)
-
 
 def get_applicant_info():
     """Prompt dialog to get the applicant's financial information.
@@ -94,37 +92,26 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
     """
 
     # Calculate the monthly debt ratio
-    # modified print-f statement to use more descriptive wording for and also to output a % instead of decimal
-    monthly_debt_ratio = calculate_monthly_debt_ratio(debt, income)
+    monthly_debt_ratio = calculate_monthly_debt_ratio(debt, income) # modified print-f below to show % output for DTI ratio
     print(f"\nYour current monthly 'Debt to Income' ratio is {100 * monthly_debt_ratio:.02f}%\n")
     
     # Calculate loan to value ratio
-    # modified print-f statement to use more descriptive wording for and also to output a % instead of decimal
     loan_to_value_ratio = calculate_loan_to_value_ratio(loan, home_value)
-    # print(f"If fully approved and accepted, this loan would increase your current 'loan to value' by {100 * loan_to_value_ratio:.02f}%.\n")
-    
+        
     # Run qualification filters
     bank_data_filtered = filter_max_loan_size(loan, bank_data)
     bank_data_filtered = filter_credit_score(credit_score, bank_data_filtered)
     bank_data_filtered = filter_debt_to_income(monthly_debt_ratio, bank_data_filtered)
     bank_data_filtered = filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
         
-    if len(bank_data_filtered) == 0:
+    if len(bank_data_filtered) == 0: # offered message of hope and encouragement below for failed applicant - and a clean errorless exit from the program below
         sys.exit("Unfortunately you did not pre-qualify for loans today; however, you may schedule a free appointment with our credit counselor.")
 
-    # # print(f"Based on your responses, you are pre-qualified for {len(bank_data_filtered)} loans.\n")
-    # else: 
-    print(f"You have qualified for {len(bank_data_filtered)} loans based on your responses.\n")
-    print(f"If fully approved and accepted, this loan would increase your current 'loan to value' by {100 * loan_to_value_ratio:.02f}%.\n")
+    print(f"You have qualified for {len(bank_data_filtered)} loans based on your responses.\n") # modified print-f below with % and CLTV increase comment
+    print(f"If fully approved, will increase your 'combined loan to value'(CLTV Ratio) by {100 * loan_to_value_ratio:.02f}%.\n")
+    print(f"Ask your loan officer about a rate/term 'cash out' refi of current mortgage, especially if drawing the new loan for debt consolidation.")
     
     return bank_data_filtered
-    # user_saves_loans = qst.confirm("Would you like to save the list of your pre-approved loan options as a (.csv) file?\n").ask()
-    # if user_saves_loans == False:
-    #     sys.exit("goodbye")
-
-    # if user_saves_loans == True:
-    #     prequal_list_path = qst.text("please enter the path to save your list as a (.csv) file:").ask()
-    
 
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
@@ -136,13 +123,12 @@ def save_qualifying_loans(qualifying_loans):
     # YOUR CODE HERE!
     user_saves_loans = qst.confirm("Would you like to save the list of your pre-approved loan options as a (.csv) file?\n").ask()
     if user_saves_loans == False:
-        sys.exit("goodbye")
+        sys.exit("Thank you for checking your financing options using the desktop underwriter.  Rates, loan amounts, and borrower requirements are subject to change daily.")
 
     if user_saves_loans == True:
-        prequal_list_path = qst.text("please enter the path to save your list as a (.csv) file:").ask()
+        prequal_list_path = qst.text("please enter the path to save your list as a (.csv) file:\n").ask()
         prequal_list_path = Path(prequal_list_path)
-    print(prequal_list_path)
-    print("11111111111111")
+    
     print (f"Your loans are now saved in {prequal_list_path}.  Thank you for using the UCB FinTech Desktop Underwriter!\n")
     
     return save_csv(qualifying_loans, prequal_list_path)
@@ -161,8 +147,7 @@ def run():
     qualifying_loans = find_qualifying_loans(
         bank_data, credit_score, debt, income, loan_amount, home_value
     )
-  #  print(qualifying_loans)
-
+ 
     # Save qualifying loans
     save_qualifying_loans(qualifying_loans)
 
